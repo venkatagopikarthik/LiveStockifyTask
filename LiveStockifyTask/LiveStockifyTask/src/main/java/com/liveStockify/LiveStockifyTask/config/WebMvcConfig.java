ed8810e3613ca1e591c777b1d521dcb25c35ddbe
A.VENKATA GOPI KARTHIK <115243849+venkatagopikarthik@users.noreply.github.com>
2025-09-07 21:35:30 +0530

    Update WebMvcConfig.java

--- LiveStockifyTask/LiveStockifyTask/src/main/java/com/liveStockify/LiveStockifyTask/config/WebMvcConfig.java
+++ LiveStockifyTask/LiveStockifyTask/src/main/java/com/liveStockify/LiveStockifyTask/config/WebMvcConfig.java
@@ -10,17 +10,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Serve static files from /static/ directory
+   
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/")
                 .setCachePeriod(3600);
         
-        // Serve CSS, JS, and other static resources
         registry.addResourceHandler("/styles.css", "/script.js", "/index.html")
                 .addResourceLocations("classpath:/static/")
                 .setCachePeriod(3600);
         
-        // Serve all other static resources
+   
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .setCachePeriod(3600);
@@ -28,8 +27,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Redirect root to index.html
+       
         registry.addViewController("/").setViewName("forward:/index.html");
         registry.addViewController("/index").setViewName("forward:/index.html");
     }
 }
+
